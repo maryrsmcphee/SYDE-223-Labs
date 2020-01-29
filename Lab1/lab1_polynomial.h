4555cd3f57d18301fa7961ba70905a45c790537f
Mary McPhee <maryrsmcphee@gmail.com>
2020-01-29 11:05:27 -0500

    added zero polynomial in equals test

--- Lab1/lab1_polynomial.h
+++ Lab1/lab1_polynomial.h
@@ -77,6 +77,7 @@ class PolynomialTest{
     Polynomial PolynomialInstance13;
     Polynomial PolynomialInstance14;
     Polynomial PolynomialInstance15;
+    Polynomial PolynomialInstance16;
 public:
     friend class Polynomial;
 
