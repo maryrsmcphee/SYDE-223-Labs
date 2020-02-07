6e67173d288dcaba16be4588b5686a36e091f425
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-02-07 12:06:47 -0500

    added rest of mutators, will now begin testing

--- Lab2/lab2_drones_manager.cpp
+++ Lab2/lab2_drones_manager.cpp
@@ -13,7 +13,7 @@ DronesManager::DronesManager() {
 DronesManager::~DronesManager() {
 }
 
-bool operator==(const DronesManager::DroneRecord& lhs, const DronesManager::DroneRecord& rhs) {
+bool operator==(const DronesManager::DroneRecord &lhs, const DronesManager::DroneRecord &rhs) {
 //  TA says we only need to compare the ID but I'm skeptical
     return (lhs.droneID == rhs.droneID);
 }
@@ -27,9 +27,9 @@ bool DronesManager::empty() const {
 }
 
 DronesManager::DroneRecord DronesManager::select(unsigned int index) const {
-    if(index > size || index < 0){
+    if (index > size || index < 0) {
         return size;
-    } else if (empty()){
+    } else if (empty()) {
         return 0;
     } else {
         return DroneRecord(index);
@@ -37,20 +37,20 @@ DronesManager::DroneRecord DronesManager::select(unsigned int index) const {
 }
 
 unsigned int DronesManager::search(DroneRecord value) const {
-    for(int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         if (DroneRecord(i) == value) {
             return i;
         } else if (empty()) {
             return 0;
-        } else if (i < 0 || i > size){
+        } else if (i < 0 || i > size) {
             return size;
         }
     }
 }
 
 void DronesManager::print() const {
-    DroneRecord* cur = first;
-    for(int i = 0; i < size; i++){
+    DroneRecord *cur = first;
+    for (int i = 0; i < size; i++) {
         cout << "Drone ID = " << cur->droneID << endl;
         cout << "Year bought = " << cur->yearBought << endl;
         cout << "Range = " << cur->range << endl;
@@ -106,6 +106,7 @@ bool DronesManager::insert_back(DroneRecord value) {
     recordToInsert->prev = current;
     return current->next == recordToInsert ? true : false;
 }
+
 // TODO Sammy
 bool DronesManager::remove(unsigned int index) {
     DroneRecord *current = this->first;
@@ -124,59 +125,90 @@ bool DronesManager::remove(unsigned int index) {
         }
         current->prev = current->next;
         current = NULL;
-        delete (current);
+        delete current;
         return true;
     }
 }
+
 // TODO Sammy
 bool DronesManager::remove_front() {
-    return false;
+    if (this->first == NULL) {
+        return false;
+    } else {
+        DroneRecord *current = this->first;
+        DroneRecord *temp = current->next;
+        this->first = temp;
+        temp->prev = this->first;
+        current = NULL;
+        delete current;
+        return true;
+    }
 }
+
 // TODO Sammy
 bool DronesManager::remove_back() {
-    return false;
+    if(this->first == NULL){
+        return false;
+    }else{
+        DroneRecord *current = this->first;
+        while(current->next->next){
+            current = current->next;
+        }
+        current->next->next = NULL;
+        current->next = NULL;
+        delete current->next->next;
+        return true;
+    }
 }
+
 // TODO Mary
 bool DronesManager::replace(unsigned int index, DroneRecord value) {
     return false;
 }
+
 // TODO Mary
 bool DronesManager::reverse_list() {
     return false;
 }
+
 // TODO Sammy
 bool DronesManagerSorted::is_sorted_asc() const {
     DroneRecord *cur;
     for (int i = 0; i > size; i++) {
-        if(cur > cur->next){
+        if (cur > cur->next) {
             return false;
         }
         cur = cur->next;
     }
     return true;
 }
- // TODO Mary
+
+// TODO Mary
 bool DronesManagerSorted::is_sorted_desc() const {
     DroneRecord *cur;
     for (int i = 0; i > size; i++) {
-        if(cur < cur->next){
+        if (cur < cur->next) {
             return false;
         }
         cur = cur->next;
     }
     return true;
 }
+
 // TODO Sammy
 bool DronesManagerSorted::insert_sorted_asc(DroneRecord val) {
     return false;
 }
+
 // TODO Mary
 bool DronesManagerSorted::insert_sorted_desc(DroneRecord val) {
     return false;
 }
- // TODO Mary
+
+// TODO Mary
 void DronesManagerSorted::sort_asc() {
 }
+
 // TODO Sammy
 void DronesManagerSorted::sort_desc() {
 }
