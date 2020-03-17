3e950ca06b7dbd5254c1d37d2282f3fbd56ef6a1
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-03-17 14:47:08 -0400

    wrote exists function - still needs to be tested

--- Lab3/binary_search_tree.cpp
+++ Lab3/binary_search_tree.cpp
@@ -54,39 +54,71 @@ unsigned int BinarySearchTree::height() const {
 }
 
 // PURPOSE: Prints the contents of the tree; format not specified
-// TODO
+// TODO @Sammy
 void BinarySearchTree::print() const {
 }
 
 // PURPOSE: Returns true if a node with the value val exists in the tree
 // otherwise, returns false
-bool BinarySearchTree::exists( BinarySearchTree::TaskItem val ) const {
-    return false;
+// TODO @Sammy
+
+
+bool BinarySearchTree::exists(BinarySearchTree::TaskItem val) const {
+    if (root == NULL) {
+        cerr << "Empty tree\n";
+        return false;
+    } else if (size == 1) {
+        return val == *root ? true : false;
+    }else{
+        if(*root == val){
+            true;
+        }
+        if(root->priority < val.priority){
+            return exists(*root->right);
+        }else{
+            return exists(*root->left);
+        }
+    }
 }
 
+/*
+ * struct node* search(struct node* root, int key)
+{
+    // Base Cases: root is null or key is present at root
+    if (root == NULL || root->key == key)
+       return root;
+
+    // Key is greater than root's key
+    if (root->key < key)
+       return search(root->right, key);
+
+    // Key is smaller than root's key
+    return search(root->left, key);
+}
+ * */
 // PURPOSE: Optional helper function that returns a pointer to the root node
-BinarySearchTree::TaskItem* BinarySearchTree::get_root_node() {
+BinarySearchTree::TaskItem *BinarySearchTree::get_root_node() {
     if (!root) {
-        cerr << "Empty tree";
+        cerr << "Empty tree\n";
         return 0;
     }
     return root;
 }
 
 // PURPOSE: Optional helper function that returns the root node pointer address
-BinarySearchTree::TaskItem** BinarySearchTree::get_root_node_address() {
+BinarySearchTree::TaskItem **BinarySearchTree::get_root_node_address() {
     return NULL;
 }
 
 // PURPOSE: Optional helper function that gets the maximum depth for a given node
-int BinarySearchTree::get_node_depth( BinarySearchTree::TaskItem* n ) const {
+int BinarySearchTree::get_node_depth(BinarySearchTree::TaskItem *n) const {
     return 0;
 }
 
 // PURPOSE: Inserts the value val into the tree if it is unique
 // returns true if successful; returns false if val already exists
 // TODO: insert when unique
-bool BinarySearchTree::insert( BinarySearchTree::TaskItem val ) {
+bool BinarySearchTree::insert(BinarySearchTree::TaskItem val) {
     if (exists(val)) return false;
     return false;
 }
@@ -94,7 +126,7 @@ bool BinarySearchTree::insert( BinarySearchTree::TaskItem val ) {
 // PURPOSE: Removes the node with the value val from the tree
 // returns true if successful; returns false otherwise
 // TODO: remove when found
-bool BinarySearchTree::remove( BinarySearchTree::TaskItem val ) {
+bool BinarySearchTree::remove(BinarySearchTree::TaskItem val) {
     if (!exists(val)) return false;
     return false;
 }
