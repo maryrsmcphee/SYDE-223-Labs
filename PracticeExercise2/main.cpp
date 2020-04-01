ff04ed9db2baf7321dca81c15527c692112f9a16
Mary McPhee <maryrsmcphee@gmail.com>
2020-04-01 10:03:27 -0400

    initial commit

---
+++ PracticeExercise2/main.cpp
@@ -0,0 +1,10 @@
+// SYDE 223, W2020, LAB 3
+// Mary McPhee, 20717047
+// Sammy Robens-Paradise, 20719541
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
