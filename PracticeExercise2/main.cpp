5c3ab0dda5088372c66ab594e6a2f422f48ae011
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-02 18:31:20 -0700

    corrected naming convention

--- PracticeExercise2/main.cpp
+++ PracticeExercise2/main.cpp
@@ -6,35 +6,51 @@
 #include <iostream>
 using namespace std;
 // setup for test node structure to create nodes to use
-struct node
+struct BinaryTreeNode
 {
-    int data;
-    struct node *left;
-    struct node *right;
+    int value;
+    struct BinaryTreeNode *left;
+    struct BinaryTreeNode *right;
 };
 /**
  * @param data
  * @return struct node
  */
-struct node* createNode(int data)
+struct BinaryTreeNode* createNode(int data)
 {
-    struct node* node = (struct node*)malloc(sizeof(struct node));
+    struct BinaryTreeNode* node = (struct BinaryTreeNode*)malloc(sizeof(struct BinaryTreeNode));
     // set node state
-    node->data = data;
+    node->value = data;
     node->left = NULL;
     node->right = NULL;
     return(node);
 }
-void printPreOrder(struct node *root){
+void printPreOrder(struct BinaryTreeNode *root){
     // ROOT -> LEFT SUBTREE -> RIGHT SUBTREE
     if(root == NULL)
         return;
-    cout<<root->data;
+    cout<<root->value<<" ";
     printPreOrder(root->left);
     printPreOrder(root->right);
 }
+
+// function of interest
+void find_and_print_sum_of_nodes (BinaryTreeNode* T, int desired_sum, int cur_sum, string buffer) {
+    // exit if T is NULLif (!T) return;
+    // update the ongoing sum with the current value for T
+    int new_sum = cur_sum + T->value;
+    // update the current path string
+  //  string new_buffer = buffer + " " + int2ascii(T->value);
+    // if the desired sum is found, print the corresponding path
+  //  if (new_sum == desired_sum)cout << new_buffer << endl;
+    // TODO: continue down theleft subtree
+    // TODO: continue down theright subtree
+    // TODO: restart from the left subtree if buffer = “”
+    // TODO: restart from the right subtreeif buffer = “”
+}
+
 int main() {
-    struct node * root = createNode(1);
+    struct BinaryTreeNode * root = createNode(1);
     root->left = createNode(5);
     root->right = createNode(7);
     root->left->left = createNode(10);
